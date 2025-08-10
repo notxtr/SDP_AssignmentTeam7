85d58ba4bcdfc1bd4f51ecae1f58e9255dcef539
nevawins <s10258673@connect.np.edu.sg>
2025-08-10 18:18:13 +0800

    update topping

--- Topping.cs
+++ Topping.cs
@@ -6,8 +6,32 @@ using System.Threading.Tasks;
 
 namespace SDP_Assignment_Team7
 {
-    internal class Topping
+    internal abstract class Topping : MenuItem
     {
+        protected MenuItem menuItem;
 
+        public Topping(MenuItem menuItem)
+        {
+            this.menuItem = menuItem;
+        }
+
+        public virtual string dishType => menuItem.dishType;
+        public virtual double price => menuItem.price;
+
+        public virtual string getDescription()
+        {
+            return menuItem.getDescription();
+        }
+
+        public virtual double getPrice()
+        {
+            return menuItem.getPrice();
+        }
+
+        public virtual void print()
+        {
+            Console.WriteLine($"{getDescription()} - ${getPrice():0.00}");
+        }
     }
+
 }
