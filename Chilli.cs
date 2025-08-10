bc500b056a228ef07ad3324e738afb02e02869f4
nevawins <s10258673@connect.np.edu.sg>
2025-08-10 18:44:12 +0800

    onion ring and chilli classes - concrete deco

---
+++ Chilli.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDP_Assignment_Team7
+{
+    internal class Chilli : Topping
+    {
+        public Chilli(MenuItem menuItem) : base(menuItem) { }
+
+        public override string getDescription()
+        {
+            return menuItem.getDescription() + ",with Extra Chilli";
+        }
+
+        public override double getPrice()
+        {
+            return menuItem.getPrice() + 0.50;
+        }
+    }
+
+}
