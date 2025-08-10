7e16a2abda6e36ffceae559ca9ecc893c7afdc0e
nevawins <s10258673@connect.np.edu.sg>
2025-08-10 17:16:47 +0800

    dish class

--- Dish.cs
+++ Dish.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace SDP_Assignment_Team7
 {
-    internal class Dish
+    internal class Dish: FoodItem
     {
+        private string Name;
+        private double Price;
+
+        public Dish(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string getDescription()
+        {
+            return Name;
+        }
+
+        public double getPrice()
+        {
+            return Price;
+        }
     }
 }
